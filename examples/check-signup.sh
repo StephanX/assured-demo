8a01b31462a709d58fdaefaf1971721c8a91e21d
Stephan Budge <stephan@stephanx.net>
2025-02-21 09:51:22 -0800

    more cleanup, examples

---
+++ examples/check-signup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+RESP="130"
+while [ ${RESP} != 0 ] ; do
+  aws ec2 describe-regions --region=us-west-2 2> /dev/null
+  RESP=$?
+  printf .
+  sleep 5
+done
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+echo "DONE DONE DONE"
+vlc ~/DONE.mp4
